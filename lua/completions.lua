5bc59efff73e8b5ed779b7b18ec0346e62675ced
Christian Valenzuela <41182258+im2wddrf@users.noreply.github.com>
2022-01-24 21:33:05 -0800

    setup treesitter

--- lua/completions.lua
+++ lua/completions.lua
@@ -87,21 +87,33 @@ cmp.event:on(
 )
 
 require('Comment').setup()
-require('telescope').setup {}
+require('telescope').setup ()
 
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = "maintained",
+  sync_install = false,
+  ignore_install = false,
+  highlight = {
+    enable = true,
+    disable = "",
 
--- -- does not appear to work with jsx which is a deal breaker
--- -- does work for vanilla html for some reason
--- require('nvim-ts-autotag').setup({
---
---   filetypes = {
---     'html', 'javascript', 'javascriptreact', 'typescriptreact', 'svelte', 'vue'
---   },
---
---   skip_tags = {
---     'area', 'base', 'br', 'col', 'command', 'embed', 'hr', 'img', 'slot',
---     'input', 'keygen', 'link', 'meta', 'param', 'source', 'track', 'wbr','menuitem'
---   }
---
--- })
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = true,
+  },
+  autopairs = {
+    enable = true
+  },
+  indent = {
+    enable = false,
+    disable = { 'yaml', 'python', 'javascript' }
+  },
+  autotag = {
+    enable = true,
+    disable = { 'xml' }
+  }
+}
+
 
