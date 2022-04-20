c9614e01bcb93576cc662f614e8f28fd46fbdcce
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-04-20 16:58:07 -0700

    add new autocmds in lua form

--- init.lua
+++ init.lua
@@ -2,20 +2,22 @@
 require('configs')
 
 
--- use template html file
--- vim.cmd([[
--- augroup skeletons
---   au!
---   autocmd BufNewFile *.* silent! execute '0r ~/.config/nvim/templates/index.'.expand("<afile>:e")
--- augroup END
--- ]])
 
 -- vim.api.nvim_create_augroup('skeletons')
--- vim.api.nvim_create_autocmd('BufNewFile', {
---   pattern = { 'index.html' },
---   command = "silent! execute '0r ~/.config/nvim/templates/index.'.expand('<afile>:e')"
--- })
+vim.api.nvim_create_autocmd('BufNewFile', {
+  pattern = { 'index.html' },
+  command = "silent! execute '0r ~/.config/nvim/templates/index.'.expand('<afile>:e')"
+})
 
+vim.api.nvim_create_autocmd('BufNewFile', {
+  pattern = { '*.sh' },
+  command = "silent! execute '0r ~/.config/nvim/templates/script.'.expand('<afile>:e') | 3"
+})
+
+vim.api.nvim_create_autocmd('BufNewFile', {
+  pattern = { 'jsconfig.json' },
+  command = "silent! execute '0r ~/.config/nvim/templates/jsconfig.'.expand('<afile>:e')"
+})
 
 require('diagnostics')
 require('packages')
