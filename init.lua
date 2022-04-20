771424a364a45b234232670b2cfcede8d5a8a7e7
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-04-20 16:29:09 -0700

    scaffold new autocmds in lua

--- init.lua
+++ init.lua
@@ -3,13 +3,18 @@ require('configs')
 
 
 -- use template html file
-vim.cmd([[
-augroup skeletons
-  au!
-  autocmd BufNewFile *.* silent! execute '0r ~/.config/nvim/templates/index.'.expand("<afile>:e")
-augroup END
-]])
+-- vim.cmd([[
+-- augroup skeletons
+--   au!
+--   autocmd BufNewFile *.* silent! execute '0r ~/.config/nvim/templates/index.'.expand("<afile>:e")
+-- augroup END
+-- ]])
 
+-- vim.api.nvim_create_augroup('skeletons')
+-- vim.api.nvim_create_autocmd('BufNewFile', {
+--   pattern = { 'index.html' },
+--   command = "silent! execute '0r ~/.config/nvim/templates/index.'.expand('<afile>:e')"
+-- })
 
 
 require('diagnostics')
