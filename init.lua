22f49e494adfff57decab29fff41f451cd775d82
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-04-20 17:33:16 -0700

    ref augroups

--- init.lua
+++ init.lua
@@ -4,19 +4,24 @@ require('configs')
 
 
 -- vim.api.nvim_create_augroup('skeletons')
+local group_skeleton = vim.api.nvim_create_augroup('skeleton', { clear = true })
+
 vim.api.nvim_create_autocmd('BufNewFile', {
   pattern = { 'index.html' },
-  command = "silent! execute '0r ~/.config/nvim/templates/index.'.expand('<afile>:e')"
+  command = "silent! execute '0r ~/.config/nvim/templates/index.'.expand('<afile>:e')",
+  group = group_skeleton
 })
 
 vim.api.nvim_create_autocmd('BufNewFile', {
   pattern = { '*.sh' },
-  command = "silent! execute '0r ~/.config/nvim/templates/script.'.expand('<afile>:e') | 3"
+  command = "silent! execute '0r ~/.config/nvim/templates/script.'.expand('<afile>:e') | 3",
+  group = group_skeleton
 })
 
 vim.api.nvim_create_autocmd('BufNewFile', {
   pattern = { 'jsconfig.json' },
-  command = "silent! execute '0r ~/.config/nvim/templates/jsconfig.'.expand('<afile>:e')"
+  command = "silent! execute '0r ~/.config/nvim/templates/jsconfig.'.expand('<afile>:e')",
+  group = group_skeleton
 })
 
 require('diagnostics')
