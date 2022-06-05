5f594c1b3fe25a80ebb3936fd2f1c7b480baabf3
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-06-04 22:30:14 -0700

    use env vars to detect appropriate colorscheme

--- lua/pkgs/init.lua
+++ lua/pkgs/init.lua
@@ -1,7 +1,7 @@
 require('pkgs.bufferline')
 -- require('pkgs.github-themes')
 require('pkgs.lualine')
--- require('pkgs.material')
+require('pkgs.material')
 require('pkgs.nvim-tree')
 require('pkgs.comment')
 require('pkgs.telescope')
@@ -11,4 +11,15 @@ require('pkgs.treesitter')
 require('pkgs.luasnip')
 require('pkgs.floaterm')
 require('pkgs.catppuccin')
+
+
+local iterm_prof = os.getenv('ITERM_PROFILE')
+
+
+if (iterm_prof == 'Material Deep Ocean') then
+  vim.cmd([[ colorscheme material ]])
+elseif (string.match(iterm_prof, 'Catppuccin')) then
+  vim.cmd([[ colorscheme catppuccin ]])
+end
+
 
