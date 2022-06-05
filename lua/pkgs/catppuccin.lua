133b002a26ea57298e319d90cf7830f7feb9bb17
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-06-04 22:29:19 -0700

    use iterm env var to detect flavor

--- lua/pkgs/catppuccin.lua
+++ lua/pkgs/catppuccin.lua
@@ -1,9 +1,35 @@
 local catppuccin = require('catppuccin')
 
 -- flavours include dusk, latte, frappe, macchiato, mocha
-vim.g.catppuccin_flavour = "mocha"
+local iterm_profile = os.getenv('ITERM_PROFILE')
+print(iterm_profile)
+
+if (string.match(iterm_profile, 'Catppuccin')) then
+
+  if (string.match(iterm_profile, 'Frappe')) then
+    print()
+    vim.g.catppuccin_flavour = "frappe"
+  end
+
+  if (string.match(iterm_profile, 'Latte')) then
+    print()
+    vim.g.catppuccin_flavour = "latte"
+  end
+  
+  if (string.match(iterm_profile, 'Macchiato')) then
+    print()
+    vim.g.catppuccin_flavour = "macchiato"
+  end
+  
+  if (string.match(iterm_profile, 'Mocha')) then
+    print()
+    vim.g.catppuccin_flavour = "mocha"
+  end
+  
+end
+
+
+
 catppuccin.setup()
-
-
 vim.cmd[[ colorscheme catppuccin ]]
 
