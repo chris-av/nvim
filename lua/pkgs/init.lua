a5679194f88b47fac70e63c55e1963c3d4174bd2
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-06-05 23:11:03 -0700

    add embark to detectable colorschemes

--- lua/pkgs/init.lua
+++ lua/pkgs/init.lua
@@ -20,6 +20,9 @@ if (iterm_prof == 'Material Deep Ocean') then
   vim.cmd([[ colorscheme material ]])
 elseif (string.match(iterm_prof, 'Catppuccin')) then
   vim.cmd([[ colorscheme catppuccin ]])
+elseif (iterm_prof == 'Embark') then
+  vim.cmd([[ set termguicolors ]])
+  vim.cmd([[ colorscheme embark ]])
 end
 
 
