337ff62335caaf137f18c52f9b42e90f3ea2e8f9
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-06-10 23:27:07 -0700

    better folding behavior
    Major thanks to the folks in this reddit thread:
    
    https://www.reddit.com/r/neovim/comments/psl8rq/sexy_folds/
    
    u/Rafat913 for starting the discussion, u/inet-pwnZ and u/rhinotation
    for laying out the "lua way" of setting up the folding configuration

--- lua/configs/options.lua
+++ lua/configs/options.lua
@@ -29,3 +29,15 @@ vim.o.showmode = false
 vim.o.scrolloff = 8
 vim.o.sidescrolloff = 8
 vim.opt.laststatus = 3
+vim.wo.foldmethod = "expr"
+vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
+
+
+-- setting folds
+-- vim.wo.foldtext = [[substitute(getline(v:foldstart),'\\t',repeat('\ ',&tabstop),'g').'...'.trim(getline(v:foldend)) . ' (' . (v:foldend - v:foldstart + 1) . ' lines)']]
+vim.wo.foldtext = [[substitute(getline(v:foldstart),'\\t',repeat('\ ',&tabstop),'g').'...'.trim(getline(v:foldend))]]
+vim.wo.fillchars = "fold: "
+vim.wo.foldnestmax = 3
+vim.wo.foldminlines = 1
+vim.wo.foldlevel = 99
+
