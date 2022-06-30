5518c7266269a8a01b2e2d103ae2ecd15ff1e6a5
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-06-29 23:20:46 -0700

    remove deprecated options

--- lua/pkgs/nvim-tree.lua
+++ lua/pkgs/nvim-tree.lua
@@ -57,10 +57,6 @@ require'nvim-tree'.setup {
   open_on_tab         = true,
   hijack_cursor       = false,
   update_cwd          = false,
-  update_to_buf_dir   = {
-    enable = true,
-    auto_open = true,
-  },
   diagnostics = {
     enable = false,
     icons = {
@@ -93,7 +89,6 @@ require'nvim-tree'.setup {
     height = 30,
     hide_root_folder = false,
     side = 'left',
-    auto_resize = true,
     mappings = {
       custom_only = false,
       list = list
