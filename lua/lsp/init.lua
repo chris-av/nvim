a50ea47dad662bec46b1f96f02dbbf3b69c83e2e
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-07-08 20:16:14 -0700

    source new ltex.lua file

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,3 +1,5 @@
+require('nvim-lsp-installer').setup()
+
 require('lsp.keymapping')
 require('lsp.pyright')
 require('lsp.tsserver')
@@ -5,5 +7,6 @@ require('lsp.css')
 require('lsp.html')
 require('lsp.lua-lsp')
 require('lsp.bashls')
+require('lsp.ltex')
 require('lsp.zls')
 
