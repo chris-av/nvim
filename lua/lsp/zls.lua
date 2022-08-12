f5ed62e7503f2ceb7a73bf857e374c9ef87adf9b
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-08-11 23:24:11 -0700

    add config for autoformatting

--- lua/lsp/zls.lua
+++ lua/lsp/zls.lua
@@ -4,6 +4,10 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 
+-- automatic code formatting, 0 for false, 1 for true
+vim.g.zig_fmt_autosave = 0
+
+
 nvim_lsp['zls'].setup {
   on_attach = on_attach,
   capabilities = capabilities,
