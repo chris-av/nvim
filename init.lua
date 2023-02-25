f35310a26c7d3b2086fa8d750bba1c192fc10b29
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-02-24 20:53:21 -0800

    move colorscheme detection to top init.lua

--- init.lua
+++ init.lua
@@ -30,4 +30,26 @@ require('debuggers')
 require('pkgs')
 
 
+local iterm_prof = os.getenv('ITERM_PROFILE')
+
+if (iterm_prof == nil) then
+  return
+end
+
+
+if (iterm_prof == 'Material Deep Ocean') then
+  vim.cmd([[ colorscheme material ]])
+elseif (string.match(iterm_prof, 'Catppuccin')) then
+  vim.cmd([[ colorscheme catppuccin ]])
+elseif (iterm_prof == 'Embark') then
+  vim.cmd([[ colorscheme embark ]])
+elseif (iterm_prof == 'Dark') then
+  vim.cmd([[ colorscheme dark ]])
+elseif (iterm_prof == 'Coal') then
+  vim.cmd([[ colorscheme coal ]])
+elseif (iterm_prof == 'Matrix Theme') then
+  vim.cmd([[ colorscheme matrix ]])
+end
+
+
 
