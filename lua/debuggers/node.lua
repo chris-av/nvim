c170d399b27ceb0b6be49590cf06c1c59671548e
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-07-04 13:41:23 -0700

    path depends on system

--- lua/debuggers/node.lua
+++ lua/debuggers/node.lua
@@ -2,32 +2,45 @@
 local dap = require('dap')
 local dapui = require('dapui')
 
+
+local home = os.getenv('HOME')
+local my_os = vim.loop.os_uname().sysname
+
+local macos_loc = home .. '/.local/share/nvim/dapinstall/jsnode/vscode-node-debug2/out/src/nodeDebug.js'
+local linux_loc = home .. '/.local/share/nvim/mason/packages/node-debug2-adapter/out/src/nodeDebug.js'
+local use_loc = macos_loc
+
+if my_os == "Linux" then
+  use_loc = linux_loc
+end
+
+
 dap.adapters.node2 = {
-    type = 'executable',
-    command = 'node',
-    args = {
-        os.getenv('HOME') .. '/.local/share/nvim/dapinstall/jsnode/vscode-node-debug2/out/src/nodeDebug.js'
-    }
+  type = 'executable',
+  command = 'node',
+  args = {
+    use_loc
+  }
 }
 
 dap.configurations.javascript = {
-    {
-        name = 'Launch',
-        type = 'node2',
-        request = 'launch',
-        program = '${file}',
-        cwd = vim.fn.getcwd(),
-        sourceMaps = true,
-        protocol = 'inspector',
-        console = 'integratedTerminal'
-    },
-    {
-        -- For this to work you need to make sure the node process is started with the `--inspect` flag.
-        name = 'Attach to process',
-        type = 'node2',
-        request = 'attach',
-        processId = require'dap.utils'.pick_process,
-    },
+  {
+    name = 'Launch',
+    type = 'node2',
+    request = 'launch',
+    program = '${file}',
+    cwd = vim.fn.getcwd(),
+    sourceMaps = true,
+    protocol = 'inspector',
+    console = 'integratedTerminal'
+  },
+  {
+    -- For this to work you need to make sure the node process is started with the `--inspect` flag.
+    name = 'Attach to process',
+    type = 'node2',
+    request = 'attach',
+    processId = require 'dap.utils'.pick_process,
+  },
 }
 
 
@@ -37,7 +50,7 @@ local function dapAttach()
     type = 'node2',
     request = 'attach',
     cwd = vim.fn.getcwd(),
-    sourceMaps=  true,
+    sourceMaps = true,
     protocol = 'inspector',
     processId = require('dap.utils').pick_process(),
     skipFiles = { '<node_internals>/**/*.js' }
@@ -55,15 +68,12 @@ local function dapAttachToRemote()
     remoteRoot = "/home/vcap/app",
     sourceMaps = true,
     protocol = 'inspector',
-    skipFiles = {'<node_internals>/**/*.js'}
+    skipFiles = { '<node_internals>/**/*.js' }
   })
 end
 
 
 return {
-
   attach = dapAttach,
   attachToRemote = dapAttachToRemote,
-
 }
-
