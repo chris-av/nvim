2db1231ea65fc3b111dcae3190f96b3836919789
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-07-16 15:17:11 -0700

    simplify setup

--- lua/pkgs/silicon.lua
+++ lua/pkgs/silicon.lua
@@ -1,33 +1,8 @@
-local status_ok, silicon = pcall(require, 'silicon')
-local notifications = require('utils.notifications')
-if not status_ok then
-  notifications.plugin_error('silicon')
-  return
-end
-
-local home = os.getenv("HOME")
-local save_location = home .. "/Desktop/"
-
-silicon.setup({
-  font = 'Hack Nerd Font=26',
-  background = '#87f',
-  theme = 'Dracula',
-  line_number = true,
-  pad_vert = 80,
-  pad_horiz = 50,
-  output = {
-    clipboard = false,
-    format = "silicon-[year][month][day].png",
-    path = "/Users/chrisvalenzuela/Documents/Screen Recordings/",
-  },
-  watermark = {
-    text = ' @chris-av',
-  },
-  window_title = function()
-    return vim.fn.fnamemodify(vim.fn.bufname(vim.fn.bufnr()), ':~:.')
-  end,
+require('silicon').setup({
+  font = 'FantasqueSansMono Nerd Font=16',
+  theme = 'Monokai Extended',
 })
 
-
-vim.api.nvim_set_keymap('n', '<leader>ss', ':Silicon!<CR>', { noremap = true, silent = true })
+-- -- use this nifty shortcut when silicon works
+-- vim.api.nvim_set_keymap('n', '<leader>ss', ':Silicon!<CR>', { noremap = true, silent = true })
 
